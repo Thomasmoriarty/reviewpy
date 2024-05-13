fc32fdb3e0a1b3c0fd5a417526defb1da96005ea
Thomas Moriarty <38979922+Thomasmoriarty@users.noreply.github.com>
2024-05-13 16:24:13 +0100

    add

---
+++ listsSetsTuples.py
@@ -0,0 +1,16 @@
+# collection of data, data structure 
+
+# list = [] ordered, changeable. duplicates ok
+# sets = {} unordered, unchangeable, duplicates no, add/remove ok
+# tuples = () ordered, unchangeable. duplicates ok. Faster than list
+
+nums= [1,3,3,4,45,6,5,75,65,4,34,3,34,43,5]
+sentence=["hello", "how", "you", "going", "m8"]
+
+#  print(dir(nums))
+# print(help(nums))
+nums.append(100) # 100 added to end
+print(nums)
+
+count1=nums.count(3)
+print(count1)
